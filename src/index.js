6ab42f446eb3c554faa65c5dafecdc0679d10fc4
junuunes <juliana.nds19@gmail.com>
2025-01-21 09:42:30 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello Word");
+console.log("Primeiro desafio de código de Lógica de Programação")
+console.log("Classificador de nível de Herói")
+
+let nomeHeroi = "Juju Hacker das Galáxias"
+let nivelXP = 13.333
+let contador = nivelXP
+
+while ((contador <= 10.001) || (contador > 10.001)){
+    switch(nivelXP){
+        case 1: 
+        nivelXP < 1000
+        console.log("Ferro") 
+    }
+
+}
+
+
